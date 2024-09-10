34d018d4ad0129c43f2e171db9c2ea31cd396f9e
Mohammed Ibrahim <108186067+moibrahh@users.noreply.github.com>
2024-09-11 07:27:27 +0800

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
@@ -8,3 +9,9 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    form = CreateNewList
+    return render(response, "main/create.html", {"form":form})
+
+

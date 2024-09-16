a46ce68c1568da3920210e4704e012b091d99a70
mo.ibrahh <108186067+moibrahh@users.noreply.github.com>
2024-09-16 23:50:52 +0000

    new

--- newsite/urls.py
+++ newsite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
+    path("register/", v.register, name="register")
     #path('view1', include("main.urls")),
 ]
